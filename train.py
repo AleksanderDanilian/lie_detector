1aac124e5aae9d50f56fb15101e5135333ac146d
ale-d <ale-danilyan@yandex.ru>
2022-06-06 00:04:39 +0300

    Исправил несколько ошибок

--- train.py
+++ train.py
@@ -6,11 +6,11 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-import tensorflow
-from tensorflow.keras.models import Sequential, Model, load_model, save_model
-from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
-from tensorflow.keras import layers
-from tensorflow.keras.optimizers import Adam
+# from tensorflow.keras.models import Sequential, Model, load_model, save_model
+# from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+# from tensorflow.keras import layers
+# from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
 
 
 def move_csv(path_from, path_to):
@@ -50,6 +50,7 @@ def get_au_train(video_folder, save_path, path_to_OpenFace):
         print('all good')
 
     path_from = pathlib.Path(path_to_OpenFace, 'processed')
+    os.makedirs(save_path, exist_ok=True)
     move_csv(path_from, save_path)
 
 
@@ -91,16 +92,16 @@ def prepare_train_set(clipping_length, folder_path_truth_video_from, folder_path
 
 
 def train_model(X_train, X_test, y_train, y_test, model_save_path):
-    model = Sequential()
+    model = tf.keras.models.Sequential()
 
-    model.add(layers.LSTM(4, return_sequences=True))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1, activation='sigmoid'))
+    model.add(tf.keras.layers.LSTM(4, return_sequences=True))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 
-    model.compile(optimizer=Adam(0.001), loss='binary_crossentropy', metrics='Accuracy')
+    model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='binary_crossentropy', metrics='Accuracy')
 
-    r_lr_callback = ReduceLROnPlateau(
+    r_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(
         monitor="val_loss",
         factor=0.1,
         patience=5,
@@ -110,20 +111,20 @@ def train_model(X_train, X_test, y_train, y_test, model_save_path):
         cooldown=0,
         min_lr=0,
     )
-    ch_pt_callback = ModelCheckpoint(
-        '/content/my_model',
-        monitor="val_loss",
-        verbose=0,
-        save_best_only=True,
-        save_weights_only=False,
-        mode="auto",
-        save_freq="epoch",
-    )
+    # ch_pt_callback = tf.keras.callbacks.ModelCheckpoint(
+    #     '/content/my_model',
+    #     monitor="val_loss",
+    #     verbose=0,
+    #     save_best_only=True,
+    #     save_weights_only=False,
+    #     mode="auto",
+    #     save_freq="epoch",
+    # )
 
     model.fit(X_train, y_train, batch_size=32, epochs=60, validation_data=(X_test, y_test),
-              callbacks=[r_lr_callback, ch_pt_callback])
+              callbacks=[r_lr_callback])
 
-    model.save(model_save_path)
+    model.tf.keras.models.save(model_save_path)
 
     return model
 
